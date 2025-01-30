46dde77f110ff40283c9e6b3bf59381af1dc02f2
RishavOjha-hnh <rishavojha1507@gmail.com>
2025-01-30 17:30:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const navLinks = document.querySelector('.nav-links');
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('nav-active');
 });
+<script>
+function navigateToPage() {
+    var dropdown = document.getElementById("about-dropdown");
+    var selectedValue = dropdown.value;
+    if (selectedValue) {
+        // Redirects to the selected URL
+        window.location.href = selectedValue;
+    }
+}
+</script>
